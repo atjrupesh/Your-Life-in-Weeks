a0b371db3390cf96a5b72d7d68b6f763bc68840b
Atj Rupesh <62754636+atjrupesh@users.noreply.github.com>
2021-04-05 12:06:11 +0530

    Update main.py

--- day-2-3-exercise-1/main.py
+++ day-2-3-exercise-1/main.py
@@ -1,8 +1,4 @@
-# 🚨 Don't change the code below 👇
 age = int(input("What is your current age? "))
-# 🚨 Don't change the code above 👆
-
-#Write your code below this line 👇
 
 new= 90-age
 n_days= str((new)*365)
@@ -19,11 +15,8 @@ your_life = print(f"You have {n_days} days , {n_weeks} weeks , {n_months} months
 ################################################################################################################
 
 
-# 🚨 Don't change the code below 👇
 age = int(input("What is your current age? "))
-# 🚨 Don't change the code above 👆
 
-#Write your code below this line 👇
 
 new= 100-age
 n_days= str((new)*365)
