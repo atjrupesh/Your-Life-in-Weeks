9b2831e3a55c6d6c1432bf0ea1f1b53a0e1ed3d7
Atj Rupesh <62754636+atjrupesh@users.noreply.github.com>
2021-04-05 12:03:30 +0530

    Add files via upload

---
+++ day-2-3-exercise-1/main.py
@@ -0,0 +1,41 @@
+# 🚨 Don't change the code below 👇
+age = int(input("What is your current age? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+new= 90-age
+n_days= str((new)*365)
+# print(n_age)
+
+n_weeks= str((new)*52)
+# print(n_weeks)
+
+n_months= str((new)*12)
+# print(n_months)
+
+your_life = print(f"You have {n_days} days , {n_weeks} weeks , {n_months} months")
+
+################################################################################################################
+
+
+# 🚨 Don't change the code below 👇
+age = int(input("What is your current age? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+new= 100-age
+n_days= str((new)*365)
+# print(n_age)
+
+n_weeks= str((new)*52)
+# print(n_weeks)
+
+n_months= str((new)*12)
+# print(n_months)
+
+your_life = print(f"You have {n_days} days , {n_weeks} weeks , {n_months} months")
+
+
+
